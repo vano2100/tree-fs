33ca5530e3a1888016dc49749fe07225e75c5d01
Rudenko Ivan <rudenko_ia@outlook.com>
2019-03-19 13:43:21 +0900

    v 1.0

---
+++ tree.rb
@@ -0,0 +1,31 @@
+#!ruby
+
+def prefix(char, level)
+    char * level
+  end
+  
+  def print_element(element, level)
+    print prefix(" ", level), element, "\n"
+  end
+  
+  def walk(dir, level)
+    Dir.chdir(dir)
+    level = level + 1
+      ls = Dir.entries(".")
+      ls.each do |el|
+        # Пропускаем текущий и родительский каталоги
+        if (el == ".") || (el == "..")
+          next
+        end
+        
+        print_element(el, level)
+      
+        if File.directory? el
+          walk(el, level)
+        end
+      end
+  
+    Dir.chdir("..")
+  end
+  
+  walk(".", 0)
