30c61d22479e26c79f1ec66b9090bffab2611407
Rudenko Ivan <rudenko_ia@outlook.com>
2019-03-19 14:45:24 +0900

    Оформление

--- tree.rb
+++ tree.rb
@@ -2,30 +2,30 @@
 
 def prefix(char, level)
     char * level
-  end
+end
   
-  def print_element(element, level)
+def print_element(element, level)
     print prefix(" ", level), element, "\n"
-  end
+end
   
-  def walk(dir, level)
+def walk(dir, level)
     Dir.chdir(dir)
     level = level + 1
-      ls = Dir.entries(".")
-      ls.each do |el|
+    ls = Dir.entries(".")
+    ls.each do |el|
         # Пропускаем текущий и родительский каталоги
         if (el == ".") || (el == "..")
-          next
+            next
         end
-        
+            
         print_element(el, level)
-      
+        
         if File.directory? el
-          walk(el, level)
+            walk(el, level)
         end
-      end
+    end
   
     Dir.chdir("..")
-  end
-  
-  walk(".", 0)
+end
+
+walk(".", 0)
